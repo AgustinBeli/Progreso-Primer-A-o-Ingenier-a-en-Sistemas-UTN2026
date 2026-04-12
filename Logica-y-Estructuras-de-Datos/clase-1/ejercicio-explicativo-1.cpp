bb4caf8e7ee7ae32a92bdfd7c6d2da00050b3da4
Agustin Belizan <agustin.belizan@icloud.com>
2026-04-12 20:58:21 -0300

    Subida del primer ejercicio explicativo

---
+++ Logica-y-Estructuras-de-Datos/clase-1/ejercicio-explicativo-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+    cout << "Cual es tu nombre?" << endl;
+
+    string nombre;
+
+    cin >> nombre;
+    cout << "Hola "<< nombre << " un gusto conocerte!" << endl;
+
+    return 0;
+}
